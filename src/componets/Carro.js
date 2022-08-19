d63c761fdae486c375389c6b018d6f9a02e63386
rodriguezal3x <alexjosuerm1995@gmail.com>
2022-08-18 22:57:24 -0600

    develop

--- src/componets/Carro.js
+++ src/componets/Carro.js
@@ -20,7 +20,7 @@ const styles = {
 
 class Carro extends Component{
     render(){
-        const { carro } = this.props;
+        const { carro, esCarroVisible, mostrarCarro} = this.props;
         const cantidad = carro.reduce((acc, el) => acc + el.cantidad, 0 )
         return(
             <div>
@@ -29,10 +29,10 @@ class Carro extends Component{
                     ? <BubbleAlert value={cantidad}/>
                     : null}
                 </span>
-                <button style={styles.carro}>
+                <button onClick={mostrarCarro} style={styles.carro}>
                     Carro
                 </button>
-                <DetallesCarro carro={carro}/>
+                {esCarroVisible ? <DetallesCarro carro={carro}/> : null }
             </div>
         )
     }
--- src/componets/DetallesCarro.js
+++ src/componets/DetallesCarro.js
@@ -10,12 +10,33 @@ const styles = {
         width: '300px',
         right: 50,
     },
+    ul:{
+        margin: 0,
+        padding: 0,
+    },
+    producto:{
+        listStyleType: 'none',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '25px 20px',
+        borderBottom: '1px solid #aaa',
+    },
 }
 class DetallesCarro extends Component { 
     render() { 
+        const { carro } = this.props
+        console.log(carro)
         return(  
             <div style={styles.detallesCarro}>
-                Chanchito Feliz
+                <ul style={styles.ul}>
+                    {carro.map(x => 
+                        <li style={styles.producto} key={x.name}> 
+                            <img alt={x.name} src={x.img} width='50' height='32' />
+                            {x.name} <span>{ x.cantidad } </span>
+                        </li>
+                    )}
+                </ul>
             </div>
         )
     }
